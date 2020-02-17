7d5fada1855d1929b40c54a6a80147bf0d029f7a
Tamas Balint <tamasbalint@tamass-mbp-2.lan>
2020-02-17 22:29:14 +0000

    Transform returned variant and columns to the camel case format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { mockFetch } from '../src/back-end/server';
+import { snakeToCamelCase } from '../src/util/snakeToCamelCase';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App" />
-  );
+class App extends React.Component {
+
+  async componentDidMount() {
+    
+    try {
+      const variant = await mockFetch('/variant');
+
+      const res = snakeToCamelCase(variant);
+      console.log({res});
+
+    } catch (error) {
+
+    }
+
+    try {
+      const columns = await mockFetch('/columns');
+
+      const res = snakeToCamelCase(columns);
+      console.log({res});
+
+    } catch (error) {
+
+    }
+    
+  }
+
+  render() {
+    return <div className="App"/>;
+  }
+
 }
 
 export default App;
